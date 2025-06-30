15cb65bf4f12e3822c8e06b993d5c0f8b7fd7838
Thomas Lant <tom@lant.uk>
2025-06-30 23:47:32 +0100

    Yes! Let's make these things TomlSerializable, with the power of _vibes_. I _will_ understand what's happening here later, I promise...

--- src/bindings.rs
+++ src/bindings.rs
@@ -105,4 +105,67 @@ impl PyIntent {
         self.inner.hash(&mut hasher);
         hasher.finish()
     }
+
+
+    fn as_dict(&self) -> PyResult<Py<PyDict>> {
+        Python::with_gil(|py| {
+            let d = PyDict::new(py);
+            if let Some(alias) = &self.inner.alias {
+                d.set_item("alias", alias)?;
+            }
+            if let Some(role) = &self.inner.role {
+                d.set_item("role", role)?;
+            }
+            if let Some(objective) = &self.inner.objective {
+                d.set_item("objective", objective)?;
+            }
+            if let Some(action) = &self.inner.action {
+                d.set_item("action", action)?;
+            }
+            if let Some(subject) = &self.inner.subject {
+                d.set_item("subject", subject)?;
+            }
+            d.set_item("trackers", self.inner.trackers.clone())?;
+            Ok(d.into())
+        })
+    }
+
+    fn __getstate__(&self) -> PyResult<Py<PyDict>> {
+        self.as_dict()
+    }
+
+    fn __repr__(&self) -> PyResult<String> {
+        Ok(format!(
+            "Intent(alias={:?}, role={:?}, objective={:?}, action={:?}, subject={:?}, trackers={:?})",
+            self.inner.alias,
+            self.inner.role,
+            self.inner.objective,
+            self.inner.action,
+            self.inner.subject,
+            self.inner.trackers,
+        ))
+    }
+
+    fn __str__(&self) -> PyResult<String> {
+        self.__repr__()
+    }
+
+    // XXX: I'm acutely aware that I do not know what this is or how it works.
+    // It _is_ needed, however.
+    fn __reduce__(&self, py: Python) -> PyResult<(PyObject, (Option<String>, Option<String>, Option<String>, Option<String>, Option<String>, Vec<String>))> {
+        let intent_type = py.get_type::<Self>();
+        Ok((
+            intent_type.into(),
+            (
+                self.inner.alias.clone(),
+                self.inner.role.clone(),
+                self.inner.objective.clone(),
+                self.inner.action.clone(),
+                self.inner.subject.clone(),
+                self.inner.trackers.clone(),
+            )
+        ))
+    }
+
+
 }
